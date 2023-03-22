ebdcc3f2849806469c329ba895d1dc0d17dafe63
Jack Pickard <76234477+jackpickard1985@users.noreply.github.com>
2023-03-22 16:51:10 +0000

    Update logistic_regression.r

--- logistic_regression.r
+++ logistic_regression.r
@@ -9,15 +9,11 @@ data_frame <- as.data.frame(ML_data_train)
 if(!require(glmnet)) {install.packages("glmnet")}
 library(glmnet)
 
-X_data <- subset(ML_data_train, select = - c(death_in_6h)) 
-
-Y_data <- subset(ML_data_train, select = c(death_in_6h))
-
 data_frame[] <- lapply(data_frame, function(x) {
 
 if(is.factor(x)) as.numeric(as.character(x)) else x})
 
-X_data_2 <- data.matrix(subset(data_frame, select = -  c(death_in_6h)))    # this was changed from as.matrix as was getting an error
+X_data_2 <- data.matrix(subset(data_frame, select = -  c(death_in_6h)))    # changed from as.matrix to avoid error
 
 Y_data_2 <- data.matrix(subset(data_frame, select = c(death_in_6h)))
 
@@ -27,7 +23,7 @@ Y_data_2 <- data.matrix(subset(data_frame, select = c(death_in_6h)))
 
 print("Optimising lambda constant")
 
-set.seed(12345) # possible random initalisation?
+set.seed(12345) # random initalisation
 
 cvfit <- cv.glmnet(X_data_2, Y_data_2, alpha = 1, family = "binomial")     # alpha: o for ridge regression, 1 for LASSO regression
 
