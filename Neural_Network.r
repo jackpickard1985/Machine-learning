694f4cd655b79f2254b9218b159d32f8df863f9b
Jack Pickard <76234477+jackpickard1985@users.noreply.github.com>
2023-03-28 17:38:45 +0100

    Update Neural_Network.r

--- Neural_Network.r
+++ Neural_Network.r
@@ -14,9 +14,9 @@ data_frame[] <- lapply(data_frame, function(x) {     # unregularised logistic re
 
 if(is.factor(x)) as.numeric(as.character(x)) else x})
 
-X_data_2 <- data.matrix(subset(data_frame, select = -  c(death_in_6h)))    # this was changed from as.matrix as was getting an error
+X_data_2 <- data.matrix(subset(data_frame, select = -  c(outcome_column)))    # this was changed from as.matrix as was getting an error
 
-Y_data_2 <- data.matrix(subset(data_frame, select = c(death_in_6h)))
+Y_data_2 <- data.matrix(subset(data_frame, select = c(outcome_column)))
 
 
 
@@ -30,12 +30,12 @@ library(neuralnet)
 
 n <- cbind(X_data_2, Y_data_2)   #from datascienceplus.com/fitting-neural-network-in-r/
 
-#f <- as.formula(paste("death_in_6h ~", paste(n[!n %in% "death_in_6h"], collapse = " + ")))    #pastes all factors separated by a +
-#f <- as.formula(paste("death_in_6h ~", "HR_above + HR_below + RR_above + RR_below + FiO2 + inverse_SATs")) #small scale test
+#f <- as.formula(paste("outcome_column ~", paste(n[!n %in% "outcome_column"], collapse = " + ")))    #pastes all factors separated by a +
+#f <- as.formula(paste("outcome_column ~", "HR_above + HR_below + RR_above + RR_below + FiO2 + inverse_SATs")) #small scale test
 
 nnames <- colnames(X_data_2)
 
-f <- as.formula(paste("death_in_6h ~", paste(nnames[!nnames %in% "death_in_6h"], collapse = " + ")))
+f <- as.formula(paste("outcome_column ~", paste(nnames[!nnames %in% "outcome_column"], collapse = " + ")))
 
 hidden_nn <- c(100, 100)   # HIDDEN LAYERS
 
@@ -52,8 +52,8 @@ print("Plotting network")
 print("Generating predictions in test set")
 # see datacamp.com/community/tutorials/neural-network-models-r
 
-nn_test_X <- data.frame(subset(ML_data_test, select = - c(death_in_6h)))
-nn_test_Y <- data.frame(ML_data_test[,"death_in_6h"])
+nn_test_X <- data.frame(subset(ML_data_test, select = - c(outcome_column)))
+nn_test_Y <- data.frame(ML_data_test[,"outcome_column"])
 nn_test <- cbind(nn_test_X, nn_test_Y)
 
 nn_test[] <- lapply(data_frame, function(x) {   #temporarily replacing nn_test with 
