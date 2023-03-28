a0c73cc2131863c592277f78c2ec0239197588d6
Jack Pickard <76234477+jackpickard1985@users.noreply.github.com>
2023-03-28 17:20:17 +0100

    Update xgboost.r

--- xgboost.r
+++ xgboost.r
@@ -13,25 +13,3 @@ bst <- xgboost(data = dtrain, max.depth = 2, eta = 1, nthread = 2, nrounds = 7, 
 
 pred = predict(bst, as.matrix(subset(ML_data_test, select = - c(death_in_6h))))
 # generated predictions
-
-
-
-print("Assessing model against test set; calculating AUC")
-
-outcomes <- ML_data_test[,"death_in_6h"]  # outcomes taken as the true outcoms from the validation set
-ML_data_test_temp <- subset(ML_data_test, select = - c(death_in_6h))
-
-predictions <- pred
-
-#death_in_6h <- outcomes
-#ML_data_test <- ML_data <- cbind(ML_data_test, death_in_6h)
-#plot.roc(outcomes, predictions, print.auc=TRUE)
-
-rocobj <- plot.roc(outcomes, predictions,
-                   main = paste(nrow(ML_data_train), " training examples,",nrow(ML_data_test), "tests"), 
-                   ci = TRUE,                  # compute AUC (of AUC by default)
-                   print.auc = TRUE)           # print the AUC (will contain the CI)
-ciobj <- ci.se(rocobj,                         # CI of sensitivity
-               specificities = seq(0, 1, 0.05)) # over a select set of specificities
-plot(ciobj, type = "shape", col = "#1c61b6AA")     # plot as a blue shape
-plot(ci(rocobj, of = "thresholds", thresholds = "best")) # add one threshold
