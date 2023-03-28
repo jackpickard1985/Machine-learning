8cbd2bba32e851873a954ee1cc6fc3237d8975bf
Jack Pickard <76234477+jackpickard1985@users.noreply.github.com>
2023-03-28 17:40:30 +0100

    Update xgboost.r

--- xgboost.r
+++ xgboost.r
@@ -5,11 +5,11 @@ print("Decision tree initializing")
 if(!require(xgboost)) {install.packages("xgboost")}
 library(xgboost)
 
-dtrain <- xgb.DMatrix(data = as.matrix(subset(ML_data_train, select = - c(death_in_6h))), label = as.matrix(ML_data_train[,"death_in_6h"]))
+dtrain <- xgb.DMatrix(data = as.matrix(subset(ML_data_train, select = - c(outcome_column))), label = as.matrix(ML_data_train[,"outcome_column"]))
 # this has separated x and y
 
 bst <- xgboost(data = dtrain, max.depth = 2, eta = 1, nthread = 2, nrounds = 7, objective = "binary:logistic", verbose = 2)
 # this has performed the gradient-boosted random forest algorithm
 
-pred = predict(bst, as.matrix(subset(ML_data_test, select = - c(death_in_6h))))
+pred = predict(bst, as.matrix(subset(ML_data_test, select = - c(outcome_column))))
 # generated predictions
